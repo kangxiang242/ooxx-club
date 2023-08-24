d2d15df6afc8309db426a0b25da7a481e5175e06
谢日周 <932188443@qq.com>
2023-08-24 11:07:14 +0800

    'test'

--- app/Combine/Compose.php
+++ app/Combine/Compose.php
@@ -318,7 +318,7 @@ class Compose
      * @return bool|\Illuminate\Support\Collection|mixed|\Tightenco\Collect\Support\Collection
      */
     protected function birthplaceRules($birthplace_id,$rules,$category_ids){
-
+        dd($rules);
         if($category_ids){
             foreach($rules as $rule){
                 if($rule['field'] == $birthplace_id){
@@ -328,7 +328,7 @@ class Compose
                         if($fruits->isNotEmpty()){
                             return $fruits->random(1)->toArray();
                         }else{
-                            return false;
+                            return [];
                         }
 
                     }
